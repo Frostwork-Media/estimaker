fcf14af6fbdc1577bafebe23c19bff8d1d92bee0
Rob Gordon <rob.gordon@tone-row.com>
2023-12-19 13:01:29 -0500

    Add Toast

--- app/src/lib/useCanvasKeybinds.ts
+++ app/src/lib/useCanvasKeybinds.ts
@@ -1,10 +1,35 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
+
+import { useToast } from "@/components/ui/use-toast";
 
 /**
  * This captures key events on the window when the canvas
  * is on the screen.
  */
 export function useCanvasKeybinds() {
+  const { toast } = useToast();
+
+  // upload image
+  const uploadImage = useCallback(
+    async (_file: File) => {
+      toast({
+        title: "Uploading...",
+        variant: "default",
+        duration: 3000,
+      });
+
+      // const formData = new FormData();
+      // formData.append("file", file);
+      // const res = await fetch("/api/upload", {
+      //   method: "POST",
+      //   body: formData,
+      // });
+      // const json = await res.json();
+      // return json.url;
+    },
+    [toast]
+  );
+
   useEffect(() => {
     const onKeyDown = (e: KeyboardEvent) => {
       // Check if the user tried to paste, and if so, prevent it.
@@ -17,7 +42,14 @@ export function useCanvasKeybinds() {
             return item.types.includes("image/png");
           });
           // Log it
-          console.log(image);
+          if (image) {
+            const blob = await image.getType("image/png");
+            const file = new File([blob], "image.png", {
+              type: "image/png",
+            });
+            const url = await uploadImage(file);
+            console.log(url);
+          }
 
           // If it is an image, convert it to a blob and then to a file.
         })();
@@ -27,5 +59,5 @@ export function useCanvasKeybinds() {
     return () => {
       window.removeEventListener("keydown", onKeyDown);
     };
-  }, []);
+  }, [uploadImage]);
 }
