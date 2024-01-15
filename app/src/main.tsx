bb03d300c841e9cd51d80baf93226e42ca588a57
Rob Gordon <rob.gordon@tone-row.com>
2024-01-15 11:09:52 -0500

    Continued testing

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,8 +6,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { LogRocket } from "./lib/logrocket.ts";
 
-console.log(__APP_ENV__);
 console.log(import.meta.env.VITE_VERCEL_ENV);
+console.log(import.meta.env.VITE_VERCEL_ENV === "production");
+console.log(import.meta.env.VITE_VERCEL_ENV === "preview");
 
 LogRocket.init("2tcix6/forecasting-whiteboard");
 
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __APP_ENV__: JSON.stringify(process.env.VITE_VERCEL_ENV),
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
